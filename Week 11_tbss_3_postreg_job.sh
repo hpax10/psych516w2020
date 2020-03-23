f61260239ef1b51cc3a0c999ee85fec572a492b8
hpax10 <55935223+hpax10@users.noreply.github.com>
2020-03-22 18:37:10 -0600

    Create Week 11_tbss_3_postreg_job.sh

---
+++ Week 11_tbss_3_postreg_job.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+
+#SBATCH --time=00:30:00 # walltime
+#SBATCH --ntasks=1 # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1 # number of nodes
+#SBATCH --mem-per-cpu=16384M # memory per CPU core
+
+FSLDIR=/fslhome/hpax10/research_bin/FSL
+PATH=${FSLDIR}/bin:${PATH}
+export FSLDIR PATH
+. ${FSLDIR}/etc/fslconf/fsl.sh
+
+cd ~/516/data/derivative/tbss
+tbss_3_postreg -S
